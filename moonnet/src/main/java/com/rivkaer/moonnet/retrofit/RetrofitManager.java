1add8f6e943f72a17f32d19153001b035604654c
rivkaer <rivkaer@qq.com>
2019-04-21 16:12:35 +0800

    [MoonNet] Retrofit 管理器

---
+++ moonnet/src/main/java/com/rivkaer/moonnet/retrofit/RetrofitManager.java
@@ -0,0 +1,40 @@
+package com.rivkaer.moonnet.retrofit;
+
+import com.rivkaer.moonnet.MoonNet;
+import com.rivkaer.moonnet.convert.GsonConverterFactory;
+import com.rivkaer.moonnet.okhttp.OKHttpManager;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+/**
+ * @author: Rivkaer Jia
+ * @Date: 2019/4/21
+ * @Email: cnrivkaer@outlook.com
+ * @Description: Retrofit 管理器
+ */
+public final class RetrofitManager {
+
+    private static Retrofit mRetrofit;
+
+    private RetrofitManager() {
+        Retrofit.Builder builder = new Retrofit.Builder();
+        builder.baseUrl(MoonNet.isDebug() ? MoonNet.getDebugHost() : MoonNet.getApiHost());
+        builder.addConverterFactory(GsonConverterFactory.create());
+        builder.addCallAdapterFactory(RxJava2CallAdapterFactory.create());
+        builder.client(OKHttpManager.getInstance().getHttpClick());
+        mRetrofit = builder.build();
+    }
+
+    public static RetrofitManager getInstance() {
+        return RetrofitManagerHolder.instance;
+    }
+
+    private static final class RetrofitManagerHolder {
+        static RetrofitManager instance = new RetrofitManager();
+    }
+
+    private Retrofit retrofit() {
+        return mRetrofit;
+    }
+
+}
